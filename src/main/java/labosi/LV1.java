72c00852ae929e922be561b11b6d974f3b5817c2
nesquick7777 <nesquick7777@gmail.com>
2020-10-26 13:43:59 +0100

    LV1 odrađen.

---
+++ src/main/java/labosi/LV1.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labosi;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Bole
+ */
+public class LV1 {
+
+    public static void main(String[] args) {
+
+        //1. Zadatak
+        //        System.out.println("   J    a   v     v   a\n"
+        //                         + "   J   a a   v   v   a a\n"
+        //                         + "J  J  aaaaa   V V   aaaaa\n"
+        //                         + " JJ  a     a   V   a     a ");
+        //2. Zadatak
+        //        System.out.println("Hello");
+        //        System.out.println("Bojan Drezgic");
+        //3. Zadatak
+        //        int a = 74, b=36;
+        //        System.out.println(a + b);
+        //4. Zadatak
+        //        int a = 50, 3;
+        //        System.out.println(a/b);
+        //5. Zadatak
+        //        int a = Integer.parseInt(JOptionPane.showInputDialog("Unesite prvi broj")), b=Integer.parseInt(JOptionPane.showInputDialog("Unesite drugi broj"));
+        //        System.out.println(a*b);
+        //6. Zadatak
+        //        int a = Integer.parseInt(JOptionPane.showInputDialog("Unesite prvi broj")), b=Integer.parseInt(JOptionPane.showInputDialog("Unesite drugi broj"));
+        //        System.out.println(a+b);
+        //        System.out.println(a-b);
+        //        System.out.println(a*b);
+        //        System.out.println(a/b);
+        //        System.out.println(a%b);
+    }
+}
