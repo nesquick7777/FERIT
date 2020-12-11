24e42a5438aa80a0b44aa704234d305cb7288c28
nesquick7777 <nesquick7777@gmail.com>
2020-12-11 11:17:17 +0100

    Završen 2. zadatak

--- src/main/java/labosi/LV4/Start.java
+++ src/main/java/labosi/LV4/Start.java
@@ -5,17 +5,23 @@
  */
 package labosi.LV4;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import com.github.javafaker.Faker;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  *
  * @author Bole
  */
 public class Start {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         Faker faker = new Faker();
         Student student;
         List<Student> studenti = new ArrayList<>();
@@ -24,18 +30,65 @@ public class Start {
             student.setIme(faker.name().firstName());
             student.setPrezime(faker.name().lastName());
             student.setFakultet(faker.university().name());
-            student.setGodina((int) (Math.random()* (4 - 1 + 1)) + 1);
-            student.setMaticniBroj(1234567890+i);
+            student.setGodina((int) (Math.random() * (4 - 1 + 1)) + 1);
+            student.setMaticniBroj(1234567890 + i);
             student.setSmjer(faker.job().field());
-            int rand1 = (int) (Math.random()* (5 - 1 + 1) + 1);
-            int rand2 = (int) (Math.random()* (5 - 1 + 1) + 1);
-            int rand3 = (int) (Math.random()* (5 - 1 + 1) + 1);
-            student.setProsjecna_ocjena((double)(rand1+rand2+rand3)/3);
+            int rand1 = (int) (Math.random() * (5 - 1 + 1) + 1);
+            int rand2 = (int) (Math.random() * (5 - 1 + 1) + 1);
+            int rand3 = (int) (Math.random() * (5 - 1 + 1) + 1);
+            student.setProsjecna_ocjena((double) (rand1 + rand2 + rand3) / 3);
             studenti.add(student);
         }
-         for(Student test:studenti){
-             System.out.println(test.getMaticniBroj());
-             System.out.println(test.getFakultet());
-         }
+
+        System.out.println();
+
+        FileOutputStream fileOut = new FileOutputStream("D:\\Downloads\\student.ser");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        int brojac = 0;
+        for (Student test : studenti) {
+            try {
+                out.writeObject(test);
+                brojac++;
+                System.out.println(brojac + ". " + "Serialized data is saved in Downloads");
+            } catch (IOException i) {
+                i.printStackTrace();
+            }
+        }
+        out.close();
+        fileOut.close();
+
+        student = null;
+        FileInputStream fileIn = new FileInputStream("D:\\Downloads\\student.ser");
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        double prosjek = 0;
+        for (int j = 0; j < 10; j++) {
+            try {
+                student = (Student) in.readObject();
+                prosjek += student.getProsjecna_ocjena();
+            } catch (IOException i) {
+                i.printStackTrace();
+                return;
+            } catch (ClassNotFoundException c) {
+                System.out.println("Student class not found");
+                c.printStackTrace();
+                return;
+            }
+            System.out.println(j + 1 + ".");
+            System.out.println("Deserialized Student...");
+            System.out.println("Ime: " + student.getIme());
+            System.out.println("Prezime: " + student.getPrezime());
+            System.out.println("Fakultet: " + student.getFakultet());
+            System.out.println("Smjer: " + student.getSmjer());
+            System.out.println("Godina: " + student.getGodina());
+            System.out.println("Maticni broj: " + student.getMaticniBroj());
+            System.out.println("Ocjena: " + student.getProsjecna_ocjena());
+            System.out.println("-------------------------------------------------------");
+        }
+        prosjek /= 10;
+        System.out.println("Prosjek svih studenata:");
+        System.out.println(prosjek);
+        in.close();
+        fileIn.close();
+
     }
 }
--- src/main/java/labosi/LV4/Student.java
+++ src/main/java/labosi/LV4/Student.java
@@ -1,5 +1,7 @@
 package labosi.LV4;
 
+import java.io.Serializable;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +12,7 @@ package labosi.LV4;
  *
  * @author Bole
  */
-public class Student {
+public class Student implements Serializable{
     private int maticniBroj;
     private String ime;
     private String prezime;
