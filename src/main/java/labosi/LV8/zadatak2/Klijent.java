293dbab8ed8d82aaef7dcf4169357bff9afe4241
nesquick7777 <nesquick7777@gmail.com>
2021-01-11 15:38:18 +0100

    LV8: zavrsen 2. zadatak.

--- src/main/java/labosi/LV8/zadatak2/Klijent.java
+++ src/main/java/labosi/LV8/zadatak2/Klijent.java
@@ -5,10 +5,41 @@
  */
 package labosi.LV8.zadatak2;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+
 /**
  *
  * @author Bole
  */
 public class Klijent {
-    
+
+    public static void main(String[] args) throws SocketException, IOException {
+        DatagramSocket socket = new DatagramSocket();
+        byte[] podaci = new byte[100];
+        DatagramPacket prihvatniPaket = new DatagramPacket(
+                podaci, podaci.length);
+
+        Scanner input_ = new Scanner(System.in);
+        String unos;
+        System.out.println("Poruka: ");
+        unos = input_.nextLine();
+        podaci = unos.getBytes();
+
+        DatagramPacket sPaket = new DatagramPacket(podaci, podaci.length,
+                InetAddress.getLocalHost(), 5000);
+        socket.send(sPaket);
+
+        socket.receive(prihvatniPaket);
+        System.out.println("\nPacket primljen:"
+                + "\nHost: " + prihvatniPaket.getAddress()
+                + "\nPort: " + prihvatniPaket.getPort()
+                + "\nDuljina: " + prihvatniPaket.getLength()
+                + "\nSadrži:\n\t" + new String(prihvatniPaket.getData(),
+                        0, prihvatniPaket.getLength()));
+    }
 }
--- src/main/java/labosi/LV8/zadatak2/Posluzitelj.java
+++ src/main/java/labosi/LV8/zadatak2/Posluzitelj.java
@@ -5,10 +5,45 @@
  */
 package labosi.LV8.zadatak2;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 /**
  *
  * @author Bole
  */
 public class Posluzitelj {
-    
+
+    public static void main(String[] args) throws SocketException, IOException {
+        DatagramSocket socket;
+        socket = new DatagramSocket(5000);
+        while (true) {
+            byte[] podaci = new byte[100];
+            DatagramPacket prihvatniPaket = new DatagramPacket(podaci, podaci.length);
+            socket.receive(prihvatniPaket);
+            String compare = new String(prihvatniPaket.getData(), 0, prihvatniPaket.getLength());
+            
+            if (compare.contains("psovka1")||compare.contains("psovka2")||compare.contains("psovka3")||compare.contains("uvreda1")||compare.contains("uvreda2")||compare.contains("uvreda3")) {
+                compare = compare.replace("psovka1", "*******");
+                compare = compare.replace("psovka2", "*******");
+                compare = compare.replace("psovka3", "*******");
+                compare = compare.replace("uvreda1", "*******");
+                compare = compare.replace("uvreda2", "*******");
+                compare = compare.replace("uvreda3", "*******");
+            }
+            podaci=compare.getBytes();
+            System.out.println("\nPaket primljen:"
+                    + "\nHost: " + prihvatniPaket.getAddress()
+                    + "\nPort: " + prihvatniPaket.getPort()
+                    + "\nDuljina: " + prihvatniPaket.getLength()
+                    + "\nSadrži:\n\t" + compare);
+
+            DatagramPacket sPaket = new DatagramPacket(podaci,
+                    prihvatniPaket.getLength(), prihvatniPaket.getAddress(), prihvatniPaket.getPort());
+            socket.send(sPaket);
+            System.out.println("Paket poslan\n");
+        }
+    }
 }
