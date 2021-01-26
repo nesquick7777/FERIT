16e5f39f344d99e5eb52a96dab1f201a537504f4
nesquick7777 <nesquick7777@gmail.com>
2021-01-27 00:25:13 +0100

    Kolokvij: rjesen 1. zadatak.

--- src/main/java/Kolokvij/zadatak1/Test.java
+++ src/main/java/Kolokvij/zadatak1/Test.java
@@ -13,6 +13,7 @@ public class Test extends Matrica {
 
     @Override
     public void _xo(int m, String[][] matrica) {
+        boolean stanje = false;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
                 System.out.print(matrica[i][j] + " ");
@@ -23,21 +24,26 @@ public class Test extends Matrica {
         if ("_".equals(matrica[0][0])) {
             if ("X".equals(matrica[0][1]) && "X".equals(matrica[0][2])) {
                 System.out.println("00");
+                stanje=true;
             }
             if ("X".equals(matrica[1][0]) && "X".equals(matrica[2][0])) {
                 System.out.println("00");
+                stanje=true;
             }
             if ("X".equals(matrica[1][1]) && "X".equals(matrica[2][2])) {
                 System.out.println("00");
+                stanje=true;
             }
         }
         // 01 slucaj
         if ("_".equals(matrica[0][1])) {
             if ("X".equals(matrica[0][0]) && "X".equals(matrica[0][2])) {
                 System.out.println("01");
+                stanje=true;
             }
             if ("X".equals(matrica[1][1]) && "X".equals(matrica[2][1])) {
                 System.out.println("01");
+                stanje=true;
             }
         }
 
@@ -45,12 +51,15 @@ public class Test extends Matrica {
         if ("_".equals(matrica[0][2])) {
             if ("X".equals(matrica[0][0]) && "X".equals(matrica[0][1])) {
                 System.out.println("02");
+                stanje=true;
             }
             if ("X".equals(matrica[1][2]) && "X".equals(matrica[2][2])) {
                 System.out.println("02");
+                stanje=true;
             }
             if ("X".equals(matrica[1][1]) && "X".equals(matrica[2][0])) {
                 System.out.println("02");
+                stanje=true;
             }
 
         }
@@ -58,24 +67,30 @@ public class Test extends Matrica {
         if ("_".equals(matrica[1][0])) {
             if ("X".equals(matrica[0][0]) && "X".equals(matrica[2][0])) {
                 System.out.println("10");
+                stanje=true;
             }
             if ("X".equals(matrica[1][1]) && "X".equals(matrica[1][2])) {
                 System.out.println("10");
+                stanje=true;
             }
         }
         //11 slucaj
         if ("_".equals(matrica[1][1])) {
             if ("X".equals(matrica[0][1]) && "X".equals(matrica[2][1])) {
                 System.out.println("11");
+                stanje=true;
             }
             if ("X".equals(matrica[1][0]) && "X".equals(matrica[1][2])) {
                 System.out.println("11");
+                stanje=true;
             }
             if ("X".equals(matrica[0][0]) && "X".equals(matrica[2][2])) {
                 System.out.println("11");
+                stanje=true;
             }
             if ("X".equals(matrica[0][2]) && "X".equals(matrica[2][0])) {
                 System.out.println("11");
+                stanje=true;
             }
         }
 
@@ -83,9 +98,11 @@ public class Test extends Matrica {
         if ("_".equals(matrica[1][2])) {
             if ("X".equals(matrica[0][2]) && "X".equals(matrica[2][2])) {
                 System.out.println("12");
+                stanje=true;
             }
             if ("X".equals(matrica[1][1]) && "X".equals(matrica[1][0])) {
                 System.out.println("12");
+                stanje=true;
             }
         }
 
@@ -93,12 +110,15 @@ public class Test extends Matrica {
         if ("_".equals(matrica[2][0])) {
             if ("X".equals(matrica[0][0]) && "X".equals(matrica[0][1])) {
                 System.out.println("20");
+                stanje=true;
             }
             if ("X".equals(matrica[1][1]) && "X".equals(matrica[0][2])) {
                 System.out.println("20");
+                stanje=true;
             }
             if ("X".equals(matrica[2][1]) && "X".equals(matrica[2][2])) {
                 System.out.println("20");
+                stanje=true;
             }
         }
 
@@ -106,9 +126,11 @@ public class Test extends Matrica {
         if ("_".equals(matrica[2][1])) {
             if ("X".equals(matrica[2][0]) && "X".equals(matrica[2][2])) {
                 System.out.println("21");
+                stanje=true;
             }
             if ("X".equals(matrica[0][1]) && "X".equals(matrica[1][1])) {
                 System.out.println("21");
+                stanje=true;
             }
         }
 
@@ -116,14 +138,19 @@ public class Test extends Matrica {
         if ("_".equals(matrica[2][2])) {
             if ("X".equals(matrica[2][0]) && "X".equals(matrica[2][1])) {
                 System.out.println("22");
+                stanje=true;
             }
             if ("X".equals(matrica[0][2]) && "X".equals(matrica[1][2])) {
                 System.out.println("22");
+                stanje=true;
             }
             if ("X".equals(matrica[0][0]) && "X".equals(matrica[1][1])) {
                 System.out.println("22");
+                stanje=true;
             }
         }
-
+if(!stanje){
+    System.out.println("Rijesenje za pobjedu X igraca u ovom krugu ne postoji.");
+}
     }
 }
